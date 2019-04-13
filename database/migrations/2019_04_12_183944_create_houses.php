ad8c39e8485842b4b5173ea1ceee6134243d0530
manchala paavamaani <37867789+paavamaani@users.noreply.github.com>
2019-04-14 01:59:42 +0530

    Add files via upload

---
+++ database/migrations/2019_04_12_183944_create_houses.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHouses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('houses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('address');
+            $table->text('mobile');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('houses');
+    }
+}
